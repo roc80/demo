00915be1c1cdafff0c96be143a028fe7824e61bf
lipeng <85272827+roc80@users.noreply.github.com>
2025-08-19 21:23:15 +0800

    netty demo

--- src/main/java/com/pengli/netty/NettyClient.java
+++ src/main/java/com/pengli/netty/NettyClient.java
@@ -1,12 +1,11 @@
 package com.pengli.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoop;
-import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioIoHandler;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,7 +26,16 @@ public class NettyClient {
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) {
-                        ch.pipeline().addLast(new StringEncoder());
+                        ch.pipeline()
+                                .addLast(new LineBasedFrameDecoder(1024))
+                                .addLast(new StringDecoder())
+                                .addLast(new SimpleChannelInboundHandler<String>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                        log.info(msg);
+                                    }
+                                })
+                                .addLast(new StringEncoder());
                     }
                 })
                 .connect("127.0.0.1", 8080);
@@ -37,7 +45,7 @@ public class NettyClient {
                 log.info("client connect server succeed");
                 EventLoop eventLoop = connectFuture.channel().eventLoop();
                 eventLoop.scheduleAtFixedRate(() -> {
-                    connectFuture.channel().writeAndFlush(String.format("hello, %d \n", System.currentTimeMillis() / 1000));
+                    connectFuture.channel().writeAndFlush(String.format("%d\n", System.currentTimeMillis()));
                 }, 0, 1, TimeUnit.SECONDS);
             } else {
                 log.error("client connect server failed");
--- src/main/java/com/pengli/netty/NettyServer.java
+++ src/main/java/com/pengli/netty/NettyServer.java
@@ -7,7 +7,12 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * server
@@ -21,6 +26,8 @@ public class NettyServer {
     public static void main(String[] args) {
         MultiThreadIoEventLoopGroup bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
         MultiThreadIoEventLoopGroup workerGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+        ConcurrentHashMap<Channel, List<Long>> dbMap = new ConcurrentHashMap<>();
+
         ChannelFuture bindFuture = new ServerBootstrap()
                 .group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
@@ -28,23 +35,76 @@ public class NettyServer {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline()
+                                .addLast(new StringEncoder())
                                 .addLast(new LineBasedFrameDecoder(1024))
                                 .addLast(new StringDecoder())
                                 .addLast(new SimpleChannelInboundHandler<String>() {
                                     @Override
                                     protected void channelRead0(ChannelHandlerContext ctx, String msg) {
-                                        // todo@lp 服务端编辑消息，相应给客户端
-                                        // todo@lp 服务端持久化消息，并在断开连接时，打印所有消息
                                         // todo@lp 代码优化
-                                        log.info("server received message: {}", msg);
-                                        // 需要手动将消息传递给下一个handler
-                                        ctx.fireChannelRead(msg);
+                                        long currentTimeStamp = System.currentTimeMillis();
+                                        try {
+                                            String message;
+                                            if (msg == null || (message = msg.trim()).isBlank()) {
+                                                return;
+                                            }
+                                            long timeStampAtSend = Long.parseLong(message);
+                                            long latency = currentTimeStamp - timeStampAtSend;
+                                            ctx.fireChannelRead(latency);
+                                            log.info("send message latency: {} ms.", latency);
+                                            ctx.channel().writeAndFlush("send message latency: " + latency + "ms.\n");
+                                        } catch (NumberFormatException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+                                })
+                                .addLast(new SimpleChannelInboundHandler<Long>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext ctx, Long latency) throws Exception {
+                                        List<Long> latencyList = dbMap.computeIfAbsent(ctx.channel(), k -> new ArrayList<>());
+                                        latencyList.add(latency);
                                     }
 
-                                    // 至少有一个处理异常的handler, 没有的话，pipeline会忽略异常
+                                    @Override
+                                    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                                        super.channelRegistered(ctx);
+                                        log.info("{} registered.", ctx.channel().remoteAddress());
+                                    }
+
+                                    @Override
+                                    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+                                        super.channelUnregistered(ctx);
+                                        log.info("{} unregistered.", ctx.channel().remoteAddress());
+                                    }
+
+                                    @Override
+                                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                        super.channelActive(ctx);
+                                        log.info("{} active.", ctx.channel().remoteAddress());
+                                    }
+
+                                    @Override
+                                    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                                        super.channelInactive(ctx);
+                                        log.info("{} inactive.", ctx.channel().remoteAddress());
+                                        List<Long> latencyList = dbMap.get(ctx.channel());
+                                        long total = 0L;
+                                        long avgLatency = -1L;
+                                        // todo@lp 首次发送消息，建立连接比较耗时。
+                                        for (Long l : latencyList) {
+                                            total += l;
+                                        }
+                                        if (!latencyList.isEmpty()) {
+                                             avgLatency = total / latencyList.size();
+                                        }
+                                        log.info("{} 平均发送消息延迟: {} ms", ctx.channel().remoteAddress(), avgLatency);
+                                    }
+
+                                    // 异常随着handler传播，前一个handler的异常，这一个handler也要处理
                                     @Override
                                     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
                                         super.exceptionCaught(ctx, cause);
+                                        log.error(cause.getMessage());
                                     }
                                 });
                     }
